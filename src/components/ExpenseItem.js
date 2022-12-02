2ab2b817a8bc18b7e7c60abb49378096cc82dcbf
jphustman <jphustman>
2022-12-02 09:10:34 -0900

    before starting chapter 39

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
-function ExpenseItem() {
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+
     return (
-        <div>
-            <div>November 29, 2022</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+        <div className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
