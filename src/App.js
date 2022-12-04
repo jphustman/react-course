b438b056087811295dbd335ca36516c049506775
jphustman <jphustman>
2022-12-04 10:57:29 -0900

    before starting chapter 46

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
     const expenses = [
         {
             id: 'e1',
@@ -9,7 +8,11 @@ function App() {
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
-        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        {
+            id: 'e2',
+            title: 'New TV',
+            amount: 799.49,
+            date: new Date(2021, 2, 12)},
         {
             id: 'e3',
             title: 'Car Insurance',
